d4a5527f4d598cca421f53b6eddab442a0371fdd
sirishrenukumar <sirish.renukumar@gmail.com>
2015-08-08 19:33:11 +0530

    Hello World

---
+++ 11a-hello-world/src/HelloWorld.java
@@ -0,0 +1,36 @@
+
+/**
+ * Sample class the demonstrates the simplest Java application
+ * @author sirishr
+ *
+ * public - access specifier, controls the access of this code from other parts of the application 
+ * 
+ * class - keyword, that indicates that this is a class definition. Everything resides in a class!
+ * 
+ * HelloWorld - name of the class
+ * 
+ * static - similar to static members in C++
+ * 
+ * void - no result is returned 
+ * 
+ * main - method (similar to member function in C++)
+ * 
+ * String - datatype 
+ */
+public class HelloWorld {
+
+	/**
+	 * This is a documentation comment that can be used to generate documentation (javadoc tool)
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * This is a multiline comment
+		 */
+		
+		//This is a single line comment
+
+		System.out.println("Hello world. Welcome to the world of Java");
+	}
+
+}
